4f54d277671693b61702c2301c73c6a7a8b30de5
sanjay-soni-14 <its.sanjay.soni.90@gmail.com>
2024-08-26 01:13:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Add event listeners to cards
+document.querySelectorAll('.card').forEach(card => {
+	card.addEventListener('click', () => {
+		// Redirect to another link
+		window.location.href = '(link unavailable)';
+	});
+});
